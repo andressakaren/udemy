ac9ec025e2bb07272b57ce8e999f7803578ca615
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-11-06 09:53:51 -0300

    update

--- python3/projeto_/main.py
+++ python3/projeto_/main.py
@@ -1,10 +1,22 @@
 print('oi')
 
+# caminho com o ambiente virtual Inativo: (Python global)
+# C:\Users\andre\AppData\Local\Programs\Python\Python312\python.exe
+
+# caminho com o ambiente virtual ativo:
+# C:\Users\MeuNome\OneDrive\Documentos\GitHub\udemy\python3\projeto_\venv\Scripts\python.exe
+
+
+# venv\Scripts\activate - ativar ambiente virtual
+# deactivate - desativar ambiente virtual
+
+
+# python -V - Versão do python
 # pip freeze
-# pip install "pymysql"
-# pip uninstall "pymysql"
-# pip index versions "pymysql" 
-# pip install "pymysql==1.0.1"
-# pip install "pymysql" --upgrade
-# pip freeze > requirements.txt
-# pip install -r .\requirements.txt
+# pip install "pymysql" - instalar alguma biblioteca
+# pip uninstall "pymysql" - desinstalar alguma biblioteca
+# pip index versions "pymysql" - verificar versões da biblioteca que criamos
+# pip install "pymysql==1.0.1" - instalar uma versão específica da biblioteca 
+# pip install "pymysql" --upgrade - Atualizar a biblioteca 
+# pip freeze > requirements.txt - criar um arquivo para colocar as bibliotecas instaladas e as versões
+# pip install -r .\requirements.txt - 
