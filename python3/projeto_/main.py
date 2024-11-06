f5b46b5054656509274062c1f1df546b0a7a8522
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-11-06 09:09:37 -0300

    testanto venv

---
+++ python3/projeto_/main.py
@@ -0,0 +1,10 @@
+print('oi')
+
+# pip freeze
+# pip install "pymysql"
+# pip uninstall "pymysql"
+# pip index versions "pymysql" 
+# pip install "pymysql==1.0.1"
+# pip install "pymysql" --upgrade
+# pip freeze > requirements.txt
+# pip install -r .\requirements.txt
