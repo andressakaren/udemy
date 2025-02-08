18ef772a8edb4915391365ef264c26cacffb494f
andressakaren <160805915+andressakaren@users.noreply.github.com>
2025-02-08 20:07:18 -0300

    update aula215.py

--- python3/Seção 5/aula215.py
+++ python3/Seção 5/aula215.py
@@ -1,3 +1,5 @@
+# associaçãoligação de um objeto para outro objeto
+
 class Escritor:
     def __init__(self, nome):
         self.nome = nome
@@ -8,20 +10,40 @@ class Escritor:
     def ferramenta(self):
         return self._ferramenta
     
-    @ferramenta.setter
+    @ferramenta.setter # permite modificação 
     def ferramenta(self, ferramenta):
         self._ferramenta = ferramenta
         
 class FerramentaDeEscrever:
     def __init__(self, nome):
         self.nome = nome
-        
+    
+    def escrever(self):
+        return f'{self.nome} está escrevendo...'        
 
-e1 = Escritor('luuis')
-f1 = FerramentaDeEscrever('caneta Bic')
+# e1 = Escritor('luuis')
+# f1 = FerramentaDeEscrever('caneta Bic') 
+# e1.ferramenta = 'caneta' # atribuição de um avalor a variavel ferramenta
+# print(e1.ferramenta)
+# e1.ferramenta = 'lapis'
+# print(e1.ferramenta)
+# print(f1.escrever())
+
 
 # atribuir um valor ao setter
 # e1.ferramenta = 'caneta'
 # print(e1.ferramenta)
+
+
+escritor = Escritor('Luis')
+caneta = FerramentaDeEscrever('Caneta Bic')
+maquina_escrever = FerramentaDeEscrever('Maquina')
+# escritor.ferramenta = caneta # associação 
+escritor.ferramenta = maquina_escrever
+
+print(caneta.escrever())
+print(maquina_escrever.escrever())
+print(escritor.ferramenta.escrever())
+
 
 
