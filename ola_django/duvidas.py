4ac9b430f77f8f3c32817a1a24a502d1b676a196
andressakaren <160805915+andressakaren@users.noreply.github.com>
2025-02-18 20:22:04 -0300

    create first django project

---
+++ ola_django/duvidas.py
@@ -0,0 +1,25 @@
+print('oi')
+
+# caminho com o ambiente virtual Inativo: (Python global)
+# C:\Users\andre\AppData\Local\Programs\Python\Python312\python.exe
+
+# caminho com o ambiente virtual ativo:
+# C:\Users\MeuNome\OneDrive\Documentos\GitHub\udemy\python3\projeto_\venv\Scripts\python.exe
+
+
+# venv\Scripts\activate - ativar ambiente virtual
+# deactivate - desativar ambiente virtual
+
+
+# python -V - Versão do python
+# pip freeze - mostrar o que ta instalado
+# pip install "pymysql" - instalar alguma biblioteca
+# pip uninstall "pymysql" - desinstalar alguma biblioteca
+# pip index versions "pymysql" - verificar versões da biblioteca que criamos
+# pip install "pymysql==1.0.1" - instalar uma versão específica da biblioteca 
+# pip install "pymysql" --upgrade - Atualizar a biblioteca 
+# pip freeze > requirements.txt - criar um arquivo para colocar as bibliotecas instaladas e as versões. // ATUALIZAR TAMBEM
+# pip install -r .\requirements.txt - instalar o que ta dentro do requirements.txr
+
+# Verificar o caminho do python executado
+# gcm python -syntax 
