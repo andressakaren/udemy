b093d49e6264284e1ba5c547241cfe22734351ce
andressakaren <160805915+andressakaren@users.noreply.github.com>
2025-02-19 21:12:23 -0300

    aula428 - render HTML, criar templates, instalar app e corrigindo erros

--- ola_django/blog/views.py
+++ ola_django/blog/views.py
@@ -1,12 +1,17 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 def blog(request):
     print('Olá mundo')
-    return HttpResponse('BLOG que veio do app 1')
+    return render(
+        request,
+        'blog/index.html'
+    )
 
 def exemplo(request):
     print('Olá mundo')
-    return HttpResponse('Exemplo que veio do app 1')
+    return render(
+        request,
+        'blog/exemplo.html'
+    )
--- ola_django/home/views.py
+++ ola_django/home/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
-# from django.shortcuts import render # renderizar o arquivo html
+from django.shortcuts import render  # renderizar o arquivo html
 
 # Create your views here.
 
+
 def home(request):
     print('Home')
-    return HttpResponse('HOME que veio do app criado 1')
+    return render(
+        request,
+        'home/index.html'
+    )
