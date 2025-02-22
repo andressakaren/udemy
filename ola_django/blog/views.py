8928d4b7ee1e89d56651b6c866c6bb1fcf064172
andressakaren <160805915+andressakaren@users.noreply.github.com>
2025-02-22 14:50:16 -0300

    aula442 - post unico

--- ola_django/blog/views.py
+++ ola_django/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 # from . import data
 from blog.data import posts
+from typing import Any 
+from django.http import HttpRequest
 
 # Create your views here.
 
@@ -18,15 +20,26 @@ def blog(request):
         context,
     )
 
-def post(request, id):
-    print('post', id)
-    context = {
-        'posts': posts
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = None
+    
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post    
+            break
         
+    if found_post is None:
+        raise Exception('Não EXISTE')
+        
+        
+    context = {
+        'post': found_post,
+        'title': found_post['title'] + ' - '
     }
+    
     return render(
         request,
-        'blog/index.html',
+        'blog/post.html',
         context,
     )
 
